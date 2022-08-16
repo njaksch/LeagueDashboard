6251ae41ad90a3ab1f74bb6d7e1f19f9cbd9fe99
waaynee <niclas.jaksch@gmail.com>
2022-08-16 19:26:05 +0200

    Reformatting

--- app.py
+++ app.py
@@ -48,68 +48,82 @@ class Summoner:
         self.item_gold = item_gold
         self.summoner_name = summoner_name
 
+    @staticmethod
+    def getList(game_json) -> list:
+        def formatChampionName(name: str) -> str:
+            try:
+                spos = name.index('\'')
+                formatted = name[0:spos] + name[spos + 1].lower() + name[spos + 2:]
 
-def getSummonerList(game_json) -> list[Summoner]:
-    def formatChampionName(name: str) -> str:
-        try:
-            spos = name.index('\'')
-            formatted = name[0:spos] + name[spos + 1].lower() + name[spos + 2:]
-        except ValueError:
-            formatted = name.replace(' ', '').replace('.', '').replace('\'', '')
+            except ValueError:
+                formatted = name.replace(' ', '').replace('.', '').replace('\'', '')
 
-        if SPECIAL_NAMES.__contains__(formatted):
-            return SPECIAL_NAMES[formatted]
-        return formatted
+            if SPECIAL_NAMES.__contains__(formatted):
+                return SPECIAL_NAMES[formatted]
 
-    summoners: list[Summoner] = []
-    item_json = requests.get(url=URL_ITEMS.format(current_patch), verify=False).json()
-    for playerID in range(len(game_json['allPlayers'])):
-        player_json = game_json['allPlayers'][playerID]
-        gold = 0
-        for slot in player_json['items']:
-            item_id = str(slot['itemID'])
-            gold += item_json['data'][item_id]['gold']['total']
-        try:
-            summoners[playerID].item_gold = gold
-        except IndexError:
-            summoner = Summoner(
-                champion=formatChampionName(player_json['championName']),
-                team=player_json['team'],
-                position=player_json['position'],
-                item_gold=gold,
-                summoner_name=player_json['summonerName'])
-            summoners.append(summoner)
-    return summoners
+            return formatted
 
+        summoners: list[Summoner] = []
+        item_json = requests.get(url=URL_ITEMS.format(current_patch), verify=False).json()
 
-def sortPositions(summoners) -> list[Summoner]:
-    summoners_sorted: list[Summoner] = []
-    for teamID in range(len(TEAMS)):
-        for positionID in range(len(POSITIONS)):
-            for summonerID in range(len(summoners)):
-                summoner: Summoner = summoners[summonerID]
-                team = TEAMS[teamID]
-                position = POSITIONS[positionID]
-                if summoner.team == team and summoner.position == position:
-                    summoners_sorted.append(summoner)
-                    break
-    return summoners_sorted
+        for playerID in range(len(game_json['allPlayers'])):
+            player_json = game_json['allPlayers'][playerID]
+            gold = 0
 
+            for slot in player_json['items']:
+                item_id = str(slot['itemID'])
+                gold += item_json['data'][item_id]['gold']['total']
 
-def sortMostGold(summoners) -> list[Summoner]:
-    summoners_sorted: list[Summoner] = []
-    while len(summoners) > 0:
-        max_gold: int = summoners[0].item_gold
-        max_summoner: Summoner = summoners[0]
-        for i in range(1, len(summoners)):
-            summoner: Summoner = summoners[i]
-            if summoner not in summoners_sorted:
-                if summoner.item_gold > max_gold:
-                    max_gold = summoner.item_gold
-                    max_summoner = summoner
-        summoners_sorted.append(max_summoner)
-        summoners.remove(max_summoner)
-    return summoners_sorted
+            try:
+                summoners[playerID].item_gold = gold
+
+            except IndexError:
+                summoner = Summoner(
+                    champion=formatChampionName(player_json['championName']),
+                    team=player_json['team'],
+                    position=player_json['position'],
+                    item_gold=gold,
+                    summoner_name=player_json['summonerName'])
+                summoners.append(summoner)
+
+        return summoners
+
+    @staticmethod
+    def sortPositions(summoners) -> list:
+        summoners_sorted: list[Summoner] = []
+
+        for teamID in range(len(TEAMS)):
+            for positionID in range(len(POSITIONS)):
+                for summonerID in range(len(summoners)):
+                    summoner: Summoner = summoners[summonerID]
+                    team = TEAMS[teamID]
+                    position = POSITIONS[positionID]
+
+                    if summoner.team == team and summoner.position == position:
+                        summoners_sorted.append(summoner)
+                        break
+
+        return summoners_sorted
+
+    @staticmethod
+    def sortMostGold(summoners) -> list:
+        summoners_sorted: list[Summoner] = []
+        while len(summoners) > 0:
+            max_gold: int = summoners[0].item_gold
+            max_summoner: Summoner = summoners[0]
+
+            for i in range(1, len(summoners)):
+                summoner: Summoner = summoners[i]
+
+                if summoner not in summoners_sorted:
+                    if summoner.item_gold > max_gold:
+                        max_gold = summoner.item_gold
+                        max_summoner = summoner
+
+            summoners_sorted.append(max_summoner)
+            summoners.remove(max_summoner)
+
+        return summoners_sorted
 
 
 def getTeamGoldDiffImage() -> BytesIO:
@@ -127,20 +141,24 @@ def getTeamGoldDiffImage() -> BytesIO:
     axes.tick_params(axis='y', colors=COLOR_FONT)
     plt.plot(list_time, list_diff, color='r', linewidth=2.5, linestyle='-')
     plt.plot([0, 120], [0, 0], color='k', linewidth=1, linestyle='-')
+
     if max(list_time) == 0:
         plt.xlim(0, 1)
     else:
         plt.xlim(0, max(list_time))
+
     if max(list_time) == 0:
         plt.ylim(-100, 100)
     else:
         plt.ylim(min(list_diff), max(list_diff))
+
     plt.xlabel('Minute')
     plt.ylabel('Gold Difference')
     img = BytesIO()
     plt.savefig(img)
     img.seek(0)
     plt.close(fig)
+
     return img
 
 
@@ -149,6 +167,7 @@ def getData(summoners):
     dashboard_data = []
     team_gold = [0, 0]
     team_size = int(len(summoners) / 2)
+
     for i in range(team_size):
         row = {
             'position': summoners[i].position,
@@ -160,19 +179,24 @@ def getData(summoners):
             'goldChaos': '{:,}'.format(summoners[i + team_size].item_gold),
             'goldDiff': '{:,}'.format(summoners[i].item_gold - summoners[i + team_size].item_gold)
         }
+
         if row['position'] == '':
             row['position'] = 'empty'
+
         team_gold[0] += summoners[i].item_gold
         team_gold[1] += summoners[i + team_size].item_gold
         dashboard_data.append(row)
+
     team_gold_diff = team_gold[0] - team_gold[1]
     team_data = {
         'order': '{:,}'.format(team_gold[0]),
         'chaos': '{:,}'.format(team_gold[1]),
         'diff': '{:,}'.format(team_gold_diff)
     }
+
     gold_data = []
-    summoners = sortMostGold(summoners)
+    summoners = Summoner.sortMostGold(summoners)
+
     for i in range(len(summoners)):
         # TODO: detect summoner instead of hardcoded line
         if summoners[i].summoner_name == 'waayne':
@@ -189,9 +213,11 @@ def getData(summoners):
             'gold': '{:,}'.format(summoners[i].item_gold)
         }
         gold_data.append(row)
+
     data.append(dashboard_data)
     data.append(team_data)
     data.append(gold_data)
+
     return data
 
 
@@ -200,22 +226,27 @@ def index():
     global last_data
     global list_diff
     global list_time
+
     try:
         game_json = requests.get(url=URL_LIVEGAME, verify=False).json()
-        summoners = getSummonerList(game_json)
-        summoners_pos_sorted = sortPositions(summoners)
+        summoners = Summoner.getList(game_json)
+        summoners_pos_sorted = Summoner.sortPositions(summoners)
         last_data = getData(summoners if len(summoners_pos_sorted) == 0 else summoners_pos_sorted)
         game_time = game_json['gameData']['gameTime'] / 60
         team_gold_diff = int((last_data[1]['diff'].replace(',', '')))
+
         if list_diff[-1] != team_gold_diff:
             list_diff.append(team_gold_diff)
             list_time.append(game_time)
+
         return render_template('main.html', dashboardData=last_data[0], teamData=last_data[1], goldData=last_data[2])
+
     except requests.exceptions.ConnectionError:
         if last_data is not None:
             return render_template('main.html', dashboardData=last_data[0], teamData=last_data[1],
                                    goldData=last_data[2])
         return render_template('error.html')
+
     except KeyError:
         list_diff = [0]
         list_time = [0]
